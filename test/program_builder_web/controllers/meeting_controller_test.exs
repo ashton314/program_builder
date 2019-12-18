4cbe51dbf3a6528f4d4a6b5b37e16a919e55398d
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2019-12-17 22:39:34 -0700

    Add failing tests to mark what to implement with security
    I need to restrict viewing meetings and members to the unit associated
    with the logged-in user. These tests are just stubs right now, but
    they'll be grown into tests to make sure that I'm not getting back
    more than what I should.
    
    Side note: there's some stuff on the stash that I'll probably want to
    pop and work off of.

--- test/program_builder_web/controllers/meeting_controller_test.exs
+++ test/program_builder_web/controllers/meeting_controller_test.exs
@@ -49,12 +49,13 @@ defmodule ProgramBuilderWeb.MeetingControllerTest do
   @test_unit_attrs %{name: "test unit"}
   @test_user_attrs %{unit_id: 0, username: "test", password: "test"}
 
+  def fixture(kind, attrs \\ %{})
   def fixture(:meeting, unit) do
     {:ok, meeting} = Program.create_meeting(%{@create_attrs | unit_id: unit.id})
     meeting
   end
   def fixture(:user, unit), do: ProgramBuilder.Auth.create_user!(%{@test_user_attrs | unit_id: unit.id})
-  def fixture(:unit), do: ProgramBuilder.Auth.create_unit!(@test_unit_attrs)
+  def fixture(:unit, attrs), do: ProgramBuilder.Auth.create_unit!(Map.merge(@test_unit_attrs, attrs))
 
   describe "index" do
     setup [:scaffold_auth]
@@ -75,6 +76,40 @@ defmodule ProgramBuilderWeb.MeetingControllerTest do
         get(conn, Routes.live_path(ProgramBuilderWeb.Endpoint, ProgramBuilderWeb.MeetingViewerLive, meeting.id))
       end
     end
+  end
+
+  describe "unit containment" do
+    setup [:two_users]
+
+    test "build meeting for user 1—user2 can't access", %{user1: user1, user2: user2} do
+      assert true == false
+    end
+
+    test "listing meetings doesn't get other one's meetings", %{user1: user1, user2: user2} do
+      assert true == false
+    end
+
+    test "trying to get other unit's meetings is 404", %{user1: user1, user2: user2} do
+      assert true == false
+    end
+
+    test "other unit's member list is not visible" do
+      # FIXME: move this into the member_controller_test.exs
+      assert true == false
+    end
+
+    test "unit_id is an integer" do
+      # FIXME: remove this *after* creating a migration to change the unit id column into an integer
+      assert true == false
+    end
+  end
+
+  defp two_users(_) do
+    unit1 = fixture(:unit, %{name: "test unit 1"})
+    user1 = fixture(:user, unit1)
+    unit2 = fixture(:unit, %{name: "test unit 2"})
+    user2 = fixture(:user, unit2)
+    {:ok, unit1: unit1, user1: user1, unit2: unit2, user2: user2}
   end
 
   defp scaffold_auth(%{conn: conn}) do
