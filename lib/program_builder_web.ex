41a388283fc4b0025f53dab92edaf8fbd723fb09
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2019-12-18 21:14:31 -0700

    Update NPM packages 🤮

--- lib/program_builder_web.ex
+++ lib/program_builder_web.ex
@@ -24,7 +24,7 @@ defmodule ProgramBuilderWeb do
       import Plug.Conn
       import ProgramBuilderWeb.Gettext
       alias ProgramBuilderWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView.Controller, only: [live_render: 3]
+      import Phoenix.LiveView.Controller
     end
   end
 
@@ -43,7 +43,9 @@ defmodule ProgramBuilderWeb do
       import ProgramBuilderWeb.ErrorHelpers
       import ProgramBuilderWeb.Gettext
       alias ProgramBuilderWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
+      import Phoenix.LiveView,
+	only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2,
+               live_component: 2, live_component: 3, live_component: 4]
     end
   end
 
